6100bdb69995068875fdbdeed8443c0b0cd30b7f
Rafi Indra Pramudhito Zuhayr <zuudevs@gmail.com>
2026-03-24 21:25:14 +0700

    Remove outdated files and add new content for Dart programming lessons, including arithmetic, comparison, logical operators, and control flow structures. Update list operations and provide installation guide for Dart SDK on Windows.

--- Pertemuan 5/list.dart
+++ Pertemuan 5/list.dart
@@ -1,5 +1,7 @@
 final List<int> nums = [3, 2, 1, 4, 5, 10, 7, 8, 6, 9];
-final List<String> names = ["Adi", "Budi", "Choiri", "Dedi"];
+final List<String> names = ["Ari", "Budi", "Choiri", "Dedi"];
+
+// implementasi Bubble Sort lengkap dengan mode ascending dan descending
 
 void Ascending(List<int> list, int i, int j) {
   if (list[j] > list[j + 1]) {
@@ -19,7 +21,7 @@ void Descending(List<int> list, int i, int j) {
 
 void BubbleSort(List<int> list, void Function(List<int>, int, int) sort) {
   for (int i = 0; i < list.length; i++) {
-    for (int j = 0; j < list.length - 1; j++) {
+    for (int j = 0; j < list.length - 1 - i; j++) {
       sort(list, i, j);
     }
   }
@@ -28,13 +30,34 @@ void BubbleSort(List<int> list, void Function(List<int>, int, int) sort) {
 void main() {
   print("isi dari list names\t: ${names}");
 
-  List<int> SortedNumAsc = nums;
-  List<int> SortedNumDes = nums;
+  List<int> SortedNumAsc = List.from(nums);
+  List<int> SortedNumDes = List.from(nums);
+
+  print("============== Add Element ==============");
+  print("list of nums before added\t: ${nums}");
+  nums.add(10);
+  print("list of nums after added\t: ${nums}");
+
+  print("============== Remove Element ==============");
+  print("list of nums before removed\t: ${nums}");
+  nums.remove(1);
+  print("list of nums after removed\t: ${nums}");
+
+  print("============== Shuffle Element ==============");
+  print("list of nums before Shuffled\t: ${nums}");
+  nums.shuffle();
+  print("list of nums after Shuffled\t: ${nums}");
+
   print("============== Sorting ==============");
-  print("isi dari list nums\t: ${nums}");
   BubbleSort(SortedNumAsc, Ascending);
-  print("Bubble Sort Ascending\t: $nums");
+  print("Bubble Sort Ascending\t\t: $SortedNumAsc");
   BubbleSort(SortedNumDes, Descending);
-  print("Bubble Sort Descending\t: $nums");
+  print("Bubble Sort Descending\t\t: $SortedNumDes");
+  nums.sort((a, b) => a.compareTo(b));
+  print("Sort using method\t\t: $nums");
   print("=====================================");
+
+  print("============== Find Element ==============");
+  print("Find element in list of nums\t: ${nums}");
+  print((nums.contains(10) ? "Found" : "Not Found"));
 }
